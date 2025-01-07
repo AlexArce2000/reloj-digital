8b9a9db848f51eed5b4f149c7b459df7cb86e4dc
AlexArce2000 <alexisroarce2000@gmail.com>
2025-01-07 16:57:17 -0300

    completo

---
+++ js/reloj.js
@@ -0,0 +1,26 @@
+const mostrarReloj = ()=>{
+    let fecha = new Date();
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
+    console.log(`${hr}:${min}:${seg}`)
+    document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`;
+
+    const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov','Dic'];
+    const dias = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
+    let diaSemana = dias[fecha.getDay()]
+    let dia = fecha.getDay();
+    let mes = meses[fecha.getMonth()];
+    let fechaTexto = `${diaSemana}, ${dia} ${mes} ${fecha.getFullYear()}`;
+    document.getElementById('fecha').innerHTML = fechaTexto;
+    document.getElementById('contenedor').classList.toggle('animar') 
+}
+
+setInterval(mostrarReloj,1000);
+
+const formatoHora = (hora)=>{
+    if (hora < 10) {
+        hora = '0'+hora;
+    }
+    return hora;
+}
