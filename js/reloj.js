4345978b10b78104a623b8af5d6ca068a4353a95
AlexArce2000 <alexisroarce2000@gmail.com>
2025-01-08 01:47:17 -0300

    fix

--- js/reloj.js
+++ js/reloj.js
@@ -9,7 +9,7 @@ const mostrarReloj = ()=>{
     const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov','Dic'];
     const dias = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
     let diaSemana = dias[fecha.getDay()]
-    let dia = fecha.getDay();
+    let dia = fecha.getDate();
     let mes = meses[fecha.getMonth()];
     let fechaTexto = `${diaSemana}, ${dia} ${mes} ${fecha.getFullYear()}`;
     document.getElementById('fecha').innerHTML = fechaTexto;
